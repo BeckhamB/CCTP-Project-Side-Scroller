f1b9b76a16aac729f175103292d095b79be68702
Beckham <Beckham2.Bagley@live.uwe.ac.uk>
2023-04-20 00:48:02 +0100

    Potential prefab changes

---
+++ Assets/SpawnRoom.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnRoom : MonoBehaviour
+{
+    private GameObject roomTemplate;
+    public GameObject platformPrefab;
+    private AddPlatforms[] addPlatforms;
+    private PlayerInRoom playerInRoom;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerInRoom = GetComponent<PlayerInRoom>();
+        GameObject platform = Instantiate(platformPrefab, transform.position, Quaternion.identity);
+        addPlatforms = gameObject.GetComponentsInChildren<AddPlatforms>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
